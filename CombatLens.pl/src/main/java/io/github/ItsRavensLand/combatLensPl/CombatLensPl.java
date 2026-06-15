4e672d240d92881bf3bd35db83b66ad2c2c5d8fc
ItsRavens <mahan.jr.000@gmail,com>
2026-06-15 07:34:40 +0330

    initial commit

---
+++ CombatLens.pl/src/main/java/io/github/ItsRavensLand/combatLensPl/CombatLensPl.java
@@ -0,0 +1,17 @@
+package io.github.ItsRavensLand.combatLensPl;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CombatLensPl extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
